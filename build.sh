734d924ccd9b24724862a462afc2d1274a147955
Jonas Maurus <jonas-github@maurus.net>
2026-05-25 15:24:22 +0000

    bump version and description

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ go build
 fpm \
     -s dir \
     -t deb \
-    -p opensmtpd-filter-denyrelay_0.1.1.deb \
+    -p opensmtpd-filter-denyrelay_0.1.2.deb \
     -n opensmtpd-filter-denyrelay \
-    -v "0.1.1-0" \
+    -v "0.1.2-0" \
     -m "Jonas Maurus" \
     -d "opensmtpd (>=6.8.0)" \
     -d "opensmtpd (<<8.0)" \
-    --description "Allows to limit accounts to a whitelist of allowed recipients" \
+    --description "Allows to limit accounts to a whitelist of allowed recipients or sending email only with validated agent email tokens" \
     --url "https://github.com/jdelic/opensmtpd-filter-denyrelay" \
     opensmtpd-filter-denyrelay=/usr/lib/x86_64-linux-gnu/opensmtpd/filter-denyrelay
